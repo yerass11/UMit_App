ba48ef4ef8e06748cfb690b1e6a89e0e8633c4f4
yerassyl-saiman <yerassyl.s@1fit.app>
2025-05-12 18:08:00 +0500

    [feat] add tests

---
+++ UMit/UMitTests/UMitTests.swift
@@ -0,0 +1,16 @@
+//
+//  UMitTests.swift
+//  UMitTests
+//
+//  Created by Yerassyl Saiman on 11.05.2025.
+//
+
+import Testing
+
+struct UMitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
